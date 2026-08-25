413cfd61848f4260a1a6e26a53f64066b93d0485
igor bruno <igordf2502@gmail.com>
2026-08-25 19:52:11 -0300

    pratiicando

--- praticas/pratica03/app-atividade-aula-tarefa/App.js
+++ praticas/pratica03/app-atividade-aula-tarefa/App.js
@@ -1,45 +1,75 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { rotulo_btn_cadastro_meta, rotulo_input_meta, rotulo_lista_metas } from './mensagens';
-import { Button, TextInput } from 'react-native-web';
+import { StyleSheet, Text, View, ScrollView, Button, TextInput } from 'react-native';
+import {rotulo_btn_cadastro_meta, rotulo_input_meta,rotulo_lista_metas} from './mensagens';
+import { useState } from 'react';
 
 export default function App() {
+  const [inputMetaText, setInputMetaText] = useState('');
+  const [metas, setMetas] = useState([]);
+
+  function metaInputHandler(inputText) {
+    setInputMetaText(inputText);
+  }
+
+  function adicionarMetaHandler() {
+    setMetas([...metas, inputMetaText]);
+  }
+
   return (
     <View style={styles.mainContainer}>
-      <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        flex: 1
-      }}>
-        <View style={{width: '65%'}}>
-          <TextInput style={styles.inputText}
-            placeholder={rotulo_input_meta} />
+
+      <View style={styles.formContainer}>
+        <View style={{ width: '65%' }}>
+          <TextInput
+            style={styles.inputText}
+            placeholder={rotulo_input_meta}
+            onChangeText={metaInputHandler}
+          />
         </View>
-        <View style={{width: '30%'}}>
-          <Button title={rotulo_btn_cadastro_meta} />
+
+        <View style={{ width: '30%' }}>
+          <Button
+            title={rotulo_btn_cadastro_meta}
+            onPress={adicionarMetaHandler}
+          />
         </View>
       </View>
-      <View style={styles.metaContainer}>
-        <Text>{rotulo_lista_metas}</Text>
-      </View>
+
+      <ScrollView style={styles.metaContainer}>
+        {metas.map((meta, index) => (
+          <Text key={index} style={styles.item}>
+            {meta}
+          </Text>
+        ))}
+      </ScrollView>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   mainContainer: {
     padding: 30,
     flex: 1,
-    flexDirection: 'column',
   },
+
+  formContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
   inputText: {
     borderColor: '#cccccc',
     borderWidth: 1,
   },
+
   metaContainer: {
     flex: 1,
+  },
+
+  item: {
+    margin: 8,
+    borderRadius: 5,
+    padding: 10,
+    backgroundColor: 'lightblue',
   },
 });
